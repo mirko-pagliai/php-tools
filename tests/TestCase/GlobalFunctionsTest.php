bf24e61fc14917e8753f1e66d749ca62f6098e6a
mirko-pagliai <mirko.pagliai@gmail.com>
2019-01-08 16:35:59 +0100

    fixed

--- tests/TestCase/GlobalFunctionsTest.php
+++ tests/TestCase/GlobalFunctionsTest.php
@@ -506,11 +506,12 @@ class GlobalFunctionsTest extends TestCase
      */
     public function testIsWinOnUnix()
     {
-        $this->assertIsDeprecated('is_win', 'The `is_win()` function is deprecated and will be removed in a later version. Use the `IS_WIN` constant instead');
-
         $errorReporting = error_reporting(E_ALL & ~E_USER_DEPRECATED);
         $this->assertFalse(is_win());
         error_reporting($errorReporting);
+
+        $this->expectException(Deprecated::class);
+        is_win();
     }
 
     /**
@@ -520,11 +521,12 @@ class GlobalFunctionsTest extends TestCase
      */
     public function testIsWinOnWin()
     {
-        $this->assertIsDeprecated('is_win', 'The `is_win()` function is deprecated and will be removed in a later version. Use the `IS_WIN` constant instead');
-
         $errorReporting = error_reporting(E_ALL & ~E_USER_DEPRECATED);
         $this->assertTrue(is_win());
         error_reporting($errorReporting);
+
+        $this->expectException(Deprecated::class);
+        is_win();
     }
 
     /**
