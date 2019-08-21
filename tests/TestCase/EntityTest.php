28c7fa00e657d850c7f2b112eee6e601cad9db89
mirko-pagliai <mirko.pagliai@gmail.com>
2019-08-21 12:33:54 +0200

    fixed

--- tests/TestCase/EntityTest.php
+++ tests/TestCase/EntityTest.php
@@ -48,7 +48,7 @@ class EntityTest extends TestCase
         var_dump($this->Entity);
         $dump = ob_get_contents();
         ob_end_clean();
-        $this->assertContains(get_class($this->Entity), $dump);
+        $this->assertContains(EntityExample::class, $dump);
 
         if (IS_WIN) {
             $this->markTestSkipped();
