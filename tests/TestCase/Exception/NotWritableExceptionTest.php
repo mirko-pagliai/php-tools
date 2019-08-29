0255328f4ca0a142df02240c31b5d17e25f2aa06
mirko-pagliai <mirko.pagliai@gmail.com>
2019-08-29 16:28:19 +0200

    added tests for exceptions

---
+++ tests/TestCase/Exception/NotWritableExceptionTest.php
@@ -0,0 +1,45 @@
+<?php
+
+/**
+ * This file is part of php-tools.
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright   Copyright (c) Mirko Pagliai
+ * @link        https://github.com/mirko-pagliai/php-tools
+ * @license     https://opensource.org/licenses/mit-license.php MIT License
+ */
+namespace Tools\Test\Exception;
+
+use Tools\Exception\NotWritableException;
+use Tools\TestSuite\TestCase;
+
+/**
+ * NotWritableExceptionTest class
+ */
+class NotWritableExceptionTest extends TestCase
+{
+    /**
+     * Test for the exception
+     * @test
+     */
+    public function testException()
+    {
+        $file = ROOT . 'dir' . DS . 'notWritableFile';
+        try {
+            throw new NotWritableException(null, 0, null, $file);
+        } catch (NotWritableException $e) {
+            $this->assertSame('File or directory `dir/notWritableFile` is not writable', $e->getMessage());
+            $this->assertSame($file, $e->getFilePath());
+        }
+
+        try {
+            throw new NotWritableException();
+        } catch (NotWritableException $e) {
+            $this->assertSame('File or directory is not writable', $e->getMessage());
+            $this->assertNull($e->getFilePath());
+        }
+    }
+}
