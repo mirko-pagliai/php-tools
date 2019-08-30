604c66baa39ec43a7b0e6b123580497479528c14
mirko-pagliai <mirko.pagliai@gmail.com>
2019-08-30 14:20:24 +0200

    added `FileException` and `InvalidValueException` abstract exceptions

---
+++ src/Exception/FileException.php
@@ -0,0 +1,52 @@
+<?php
+
+/**
+ * This file is part of php-tools.
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright   Copyright (c) Mirko Pagliai
+ * @link        https://github.com/mirko-pagliai/php-tools
+ * @license     https://opensource.org/licenses/mit-license.php MIT License
+ * @since       1.2.12
+ */
+namespace Tools\Exception;
+
+use Exception;
+
+/**
+ * Abstract exception for exceptions that are throwed by a file
+ */
+abstract class FileException extends Exception
+{
+    /**
+     * @var string|null
+     */
+    protected $path;
+
+    /**
+     * Constructor
+     * @param string|null $message The string of the error message
+     * @param int $code The code of the error
+     * @param \Throwable|null $previous the previous exception
+     * @param string|null $path Path of the file that throwed the exception
+     * @uses $path
+     */
+    public function __construct($message = null, $code = 0, \Throwable $previous = null, $path = null)
+    {
+        parent::__construct($message, $code, $previous);
+        $this->path = $path;
+    }
+
+    /**
+     * Gets the path of the file that throwed the exception
+     * @return string|null
+     * @uses $path
+     */
+    public function getFilePath()
+    {
+        return $this->path;
+    }
+}
---
+++ src/Exception/InvalidValueException.php
@@ -0,0 +1,52 @@
+<?php
+
+/**
+ * This file is part of php-tools.
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright   Copyright (c) Mirko Pagliai
+ * @link        https://github.com/mirko-pagliai/php-tools
+ * @license     https://opensource.org/licenses/mit-license.php MIT License
+ * @since       1.2.12
+ */
+namespace Tools\Exception;
+
+use Exception;
+
+/**
+ * Abstract exception for exceptions that are throwed by an invalid value
+ */
+abstract class InvalidValueException extends Exception
+{
+    /**
+     * @var mixed
+     */
+    protected $value = null;
+
+    /**
+     * Constructor
+     * @param string|null $message The string of the error message
+     * @param int $code The code of the error
+     * @param \Throwable|null $previous the previous exception
+     * @param mixed $value The value that throwed the exception
+     * @uses $value
+     */
+    public function __construct($message = null, $code = 0, \Throwable $previous = null, $value = null)
+    {
+        parent::__construct($message, $code, $previous);
+        $this->value = $value;
+    }
+
+    /**
+     * Gets the value that throwed the exception
+     * @return mixed
+     * @uses $value
+     */
+    public function getValue()
+    {
+        return $this->value;
+    }
+}
