ba4afcadf96441b13bb3300dd717475904b04846
mirko-pagliai <mirko.pagliai@gmail.com>
2020-10-06 14:36:57 +0200

    fixed code

--- src/TestSuite/TestCase.php
+++ src/TestSuite/TestCase.php
@@ -52,12 +52,7 @@ abstract class TestCase extends PHPUnitTestCase
      */
     public function expectExceptionMessageMatches(string $regularExpression): void
     {
-        if (!method_exists(PHPUnitTestCase::class, 'expectExceptionMessageMatches')) {
-            $this->expectExceptionMessageRegExp($regularExpression);
-
-            return;
-        }
-
-        parent::expectExceptionMessageMatches($regularExpression);
+        $methodToCall = method_exists(PHPUnitTestCase::class, 'expectExceptionMessageMatches') ? [parent::class, 'expectExceptionMessageMatches'] : [$this, 'expectExceptionMessageRegExp'];
+        call_user_func($methodToCall, $regularExpression);
     }
 }
