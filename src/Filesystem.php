f21141b66ed5201d62219819a0f8829b397ce572
mirko-pagliai <mirko.pagliai@gmail.com>
2022-09-29 17:35:08 +0200

    removed useless var

--- src/Filesystem.php
+++ src/Filesystem.php
@@ -110,7 +110,6 @@ class Filesystem extends BaseFilesystem
      * @param bool $ignoreErrors With `true`, errors will be ignored
      * @return array<array<string>> Array of nested directories and files in each directory
      * @throws \Symfony\Component\Finder\Exception\DirectoryNotFoundException
-     * @todo Check the `$skipHidden` var
      */
     public function getDirTree(string $path, $exceptions = false, bool $ignoreErrors = false): array
     {
@@ -118,10 +117,8 @@ class Filesystem extends BaseFilesystem
         $finder = new Finder();
         $exceptions = (array)(is_bool($exceptions) ? ($exceptions ? ['.'] : []) : $exceptions);
 
-        $skipHidden = false;
         $finder->ignoreDotFiles(false);
         if (in_array('.', $exceptions)) {
-            $skipHidden = true;
             unset($exceptions[array_search('.', $exceptions)]);
             $finder->ignoreDotFiles(true);
         }
